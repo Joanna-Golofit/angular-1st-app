eeda33e0b1bbe80168e4146443b72a4dea27b63a
Joanna Golofit <joanna.golofit@acxiom.com>
2025-06-25 13:46:28 +0200

    Update Angular app with roles and designations

---
+++ src/app/services/master.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { APIResponseModel } from '../components/model/interface/role';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MasterService {
+  http = inject(HttpClient);
+
+  getDesignations() {
+    return this.http.get<APIResponseModel>(
+      'https://freeapi.miniprojectideas.com/api/ClientStrive/GetAllDesignation'
+    );
+  }
+}
